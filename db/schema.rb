89ef3db8bc1910d46982a3c2d072bccdcc7d3949
Duncan Hurwood <duncan.hurwood@sky.com>
2010-02-22 21:56:06 +0000

    Added title to table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100218174045) do
+ActiveRecord::Schema.define(:version => 20100222215411) do
 
   create_table "blogs", :force => true do |t|
     t.text     "post"
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20100218174045) do
     t.string   "author"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
