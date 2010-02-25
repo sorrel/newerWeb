32eded32f8bbc4f3ce8ce5dbfc00207b974923a1
Duncan Hurwood <duncan.hurwood@sky.com>
2010-02-25 18:40:22 +0000

    Links updated

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,12 @@
 class BlogsController < ApplicationController
+  
   def show
     @blog = Blog.find(params[:id])
+    @blogs = Blog.all
   end
+  
+  def index
+    @blogs = Blog.all
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'pages', :action => 'home'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.family   '/family',   :controller => 'pages', :action => 'family'
+  
   map.resources :blogs
 
   # Sample of regular route:
@@ -42,6 +43,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect ':controller/:action/:id'
+  #map.connect ':controller/:action/:id.:format'
 end
