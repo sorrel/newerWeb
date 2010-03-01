5254e160a257d95b183ad9db920e6ed4188b6bb9
Duncan Hurwood <duncan.hurwood@sky.com>
2010-03-01 19:42:10 +0000

    Design part one

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,11 +4,13 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
     @blogs = Blog.all
     @title = @blog.title
+    @page = "blog"
   end
   
   def index
     @blogs = Blog.all
     @title = "All the blogs"
+    @page = "blog"
   end
   
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @page = "home"
   end
 
   def about
     @title = "About the site"
+    @page = "about"
   end
 
   def family
     @title = "The Hurwood Family"
+    @page = "family"
   end
 
 end
