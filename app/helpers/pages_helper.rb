fcc979950ab45379cd57bf35a317d6ea31b30a34
Duncan Hurwood <duncan.hurwood@sky.com>
2010-03-03 20:19:58 +0000

    More styles

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   
   def hurwoodgroup2005
-      image_tag("hurwoodgroup2005.png", :alt => "The Hurwoods 2005", :class => "")
+      image_tag("hurwoodgroup2005.png", :alt => "The Hurwoods 2005")
   end
 end
