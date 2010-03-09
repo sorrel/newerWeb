bbf5a35b841f0d4b97f9e9a5e1e10b2330c6dff8
Duncan Hurwood <duncan.hurwood@sky.com>
2010-03-09 21:41:22 +0000

    Updating constantly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
+nolink# This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
