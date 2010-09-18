e9125a4183dcbd1f1344be758d7b7cf0d848d41c
Duncan Hurwood <duncan.hurwood@sky.com>
2010-09-18 21:08:57 +0100

    Updated to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hurwoodweb::Application.config.secret_token = '0854875e633d9bafd8c6fb6fd3810aae13655cc753b15d22b444f0f608928f7d6de325edfe4ae3baf4403f46c42fbdabdb29e385fd25e7ac1deda0cf7cdb7522'
--- spec/factories.rb
+++
@@ -1,7 +0,0 @@
-# By using the symbol ':blog', we get Factory Girl to simulate the Blogs model.
-Factory.define :blog do |blog|
-  blog.author     "Michael Hartl"
-  blog.tags       "Test testing starting"
-  blog.title      "A first title for testing"
-  blog.post       "The moves show how an entire ecosystem of new hardware is ready to spring up around new Apple products. While Apple in recent years has spawned cottage industries of new software businesses -- particularly around its iTunes App Store -- the accessory business around its gadgets has also thrived and driven the growth of many third-party manufacturers. According to Piper Jaffray & Co. estimates iPod and iPhone accessory sales -- including cases, headphones and chargers -- totaled about $3.7 billion world-wide in 2009."
-end
